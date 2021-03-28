b0c5aee46678d629f2c1fc20fe0c038d0ae0d4e7
box-jeon <utrpanic@gmail.com>
2021-03-28 21:47:27 +0900

    Chapter5. 아이템용 모델과 마이그레이션 연계.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 import re
 
+from lists.models import Item
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -40,3 +41,21 @@ class HomePageTest(TestCase):
     def remove_csrf_tag(self, text):
         csrf_regex = r'<[^>]*csrfmiddlewaretoken[^>]*>'
         return re.sub(csrf_regex, '', text)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
