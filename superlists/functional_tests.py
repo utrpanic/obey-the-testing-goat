19d80079e04ea2ad6de32430b2ce1da2ea23cfa5
box-jeon <utrpanic@gmail.com>
2021-03-28 21:08:07 +0900

    Chapter5. FT에 헬퍼 메소드 추가.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -44,9 +49,7 @@ class NewVisitorTest(unittest.TestCase):
             expected_conditions.text_to_be_present_in_element(
                 (By.ID, 'id_list_table'), input_text1))
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: ' + input_text1, [row.text for row in rows])
+        self.check_for_row_in_list_table('1: ' + input_text1)
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다
@@ -61,10 +64,8 @@ class NewVisitorTest(unittest.TestCase):
                 (By.ID, 'id_list_table'), input_text2))
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: ' + input_text1, [row.text for row in rows])
-        self.assertIn('2: ' + input_text2, [row.text for row in rows])
+        self.check_for_row_in_list_table('1: ' + input_text1)
+        self.check_for_row_in_list_table('2: ' + input_text2)
 
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해준다
