3843595a6b3bbf314f15072ce46daaa9e3bf9609
box-jeon <utrpanic@gmail.com>
2021-03-28 22:13:24 +0900

    Chapter5. POST 후에 리다이렉션과 템플릿에 모든 작업 아이템 표시.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,7 @@
-from django.urls import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
+from django.urls import resolve
 import re
 
 from lists.models import Item
@@ -22,25 +22,45 @@ class HomePageTest(TestCase):
             self.remove_csrf_tag(expected_html)
         )
 
+    def remove_csrf_tag(self, text):
+        csrf_regex = r'<[^>]*csrfmiddlewaretoken[^>]*>'
+        return re.sub(csrf_regex, '', text)
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = '신규 작업 아이템'
-        response = home_page(request)
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'},
-            request=request
-        )
-        self.assertEqual(
-            self.remove_csrf_tag(response.content.decode()), 
-            self.remove_csrf_tag(expected_html)
-        )
 
-    def remove_csrf_tag(self, text):
-        csrf_regex = r'<[^>]*csrfmiddlewaretoken[^>]*>'
-        return re.sub(csrf_regex, '', text)
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
