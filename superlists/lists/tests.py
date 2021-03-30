a4dd287a9d14296081ffa9c5aee2126da7c6fd1d
box-jeon <utrpanic@gmail.com>
2021-03-30 23:26:03 +0900

    Chapter6. new_list 뷰 추가.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -26,31 +26,23 @@ class HomePageTest(TestCase):
         csrf_regex = r'<[^>]*csrfmiddlewaretoken[^>]*>'
         return re.sub(csrf_regex, '', text)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
+class NewListTest(TestCase):
 
-        response = home_page(request)
-
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '신규 작업 아이템')
 
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 class ListViewTest(TestCase):
 
