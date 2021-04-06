f015bcea613e17e303de6303122ec4dcf14cb4ed
box-jeon <utrpanic@gmail.com>
2021-04-06 21:33:13 +0900

    Chapter10. 단위 테스트를 두 개 파일로 나누기.

--- workspace/lists/tests/test_views.py
+++ workspace/lists/tests/test_views.py
@@ -99,30 +99,3 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
-
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.list = list_
-        first_item.save()
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.list = list_
-        second_item.save()
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '두 번째 아이템')
-        self.assertEqual(second_saved_item.list, list_)
