7cc8a8b97a06091522bec080e87d2017766546cc
box-jeon <utrpanic@gmail.com>
2021-04-08 22:51:50 +0900

    Chapter10. 리스트 뷰에서 모델 유효성 검증 구현.

--- workspace/lists/tests/test_views.py
+++ workspace/lists/tests/test_views.py
@@ -50,7 +50,7 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
+        expected_error = escape('빈 아이템을 등록할 수 없습니다')
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
@@ -110,3 +110,14 @@ class ListViewTest(TestCase):
         )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape('빈 아이템을 등록할 수 없습니다')
+        self.assertContains(response, expected_error)
--- workspace/lists/views.py
+++ workspace/lists/views.py
@@ -14,13 +14,21 @@ def new_list(request):
         item.save()
     except ValidationError:
         list_.delete()
-        error = "You can't have an empty list item"
+        error = '빈 아이템을 등록할 수 없습니다'
         return render(request, 'home.html', {'error': error})
     return redirect('/lists/%d/' % (list_.id,))
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
+
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/%d/' % (list_.id,))
-    return render(request, 'list.html', {'list': list_})
+        try:
+            item = Item.objects.create(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            item.delete()
+            error = '빈 아이템을 등록할 수 없습니다'
+    return render(request, 'list.html', {'list': list_, 'error': error})
