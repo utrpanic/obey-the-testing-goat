e0c90296e7e3b5381e13bdfcb050ac3e58869e12
box-jeon <utrpanic@gmail.com>
2021-04-13 00:49:14 +0900

    Chapter11. 모든 뷰에서 폼 사용하기. required attribute 때문에 뭔가 이상하다.

--- workspace/lists/forms.py
+++ workspace/lists/forms.py
@@ -6,6 +6,16 @@ EMPTY_LIST_ERROR = '빈 아이템을 등록할 수 없습니다'
 
 class ItemForm(forms.models.ModelForm):
 
+    # Input 태그에 required 필드 때문에
+    # 빈 값이 아예 입력이 안됨. form_action이 불리질 않는다.
+    # 그렇다고 required를 꺼버리면
+    # Form validation만 하고 저장해버리기 때문에
+    # DB에 에러 없이 저장해버림.
+    # form.is_valid()에 대해 더 알게 되면 개선하기.
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['text'].required = False
+
     class Meta:
         model = Item
         fields = ('text',)
--- workspace/lists/views.py
+++ workspace/lists/views.py
@@ -8,28 +8,20 @@ def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = '빈 아이템을 등록할 수 없습니다'
-        return render(request, 'home.html', {'error': error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        item = Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {'form': form})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
+    form = ItemForm()
     if request.method == 'POST':
-        try:
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
             item = Item.objects.create(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
             return redirect(list_)
-        except ValidationError:
-            item.delete()
-            error = '빈 아이템을 등록할 수 없습니다'
-    return render(request, 'list.html', {'list': list_, 'error': error})
+    return render(request, 'list.html', {'list': list_, "form": form})
