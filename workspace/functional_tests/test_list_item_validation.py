877b08e8942d856481f4f8ff2a360ebf22af15f2
box-jeon <utrpanic@gmail.com>
2021-04-12 22:17:23 +0900

    Chapter11. 모든 아이템의 입력 id와 name 변경. 아직 망가진 상태.

--- workspace/functional_tests/test_list_item_validation.py
+++ workspace/functional_tests/test_list_item_validation.py
@@ -8,7 +8,7 @@ class ItemValidationTest(FunctionalTest):
         # 에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려고 한다.
         # 입력 상자가 비어 있는 상태에서 엔터키를 누른다.
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         time.sleep(0.5)
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는
@@ -17,13 +17,13 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, '빈 아이템을 등록할 수 없습니다')
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다.
-        self.browser.find_element_by_id('id_new_item').send_keys('우유 사기')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('우유 사기')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         time.sleep(0.5)
         self.check_for_row_in_list_table('1: 우유 사기')
 
         # 그녀는 고의적으로 다시 빈 아이템을 등록한다.
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         time.sleep(0.5)
 
         # 리스트 페이지에 다시 에러 메시지가 표시된다.
@@ -32,8 +32,8 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, '빈 아이템을 등록할 수 없습니다')
 
         # 아이템을 입력하면 정상 동작한다.
-        self.browser.find_element_by_id('id_new_item').send_keys('tea 만들기')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('tea 만들기')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         time.sleep(0.5)
         self.check_for_row_in_list_table('1: 우유 사기')
         self.check_for_row_in_list_table('2: tea 만들기')
--- workspace/lists/tests/test_views.py
+++ workspace/lists/tests/test_views.py
@@ -43,7 +43,7 @@ class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
         self.client.post(
             '/lists/new',
-            data={'item_text': '신규 작업 아이템'}
+            data={'text': '신규 작업 아이템'}
         )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -52,20 +52,20 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post(
             '/lists/new',
-            data={'item_text': '신규 작업 아이템'}
+            data={'text': '신규 작업 아이템'}
         )
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
     def test_validation_errors_are_sent_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         expected_error = escape('빈 아이템을 등록할 수 없습니다')
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data={'item_text': ''})
+        self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
@@ -103,7 +103,7 @@ class ListViewTest(TestCase):
 
         self.client.post(
             '/lists/%d/' % (correct_list.id,),
-            data={ 'item_text': '기존 목록에 신규 아이템' }
+            data={ 'text': '기존 목록에 신규 아이템' }
         )
 
         self.assertEqual(Item.objects.count(), 1)
@@ -117,7 +117,7 @@ class ListViewTest(TestCase):
 
         response = self.client.post(
             '/lists/%d/' % (correct_list.id,),
-            data={ 'item_text': '기존 목록에 신규 아이템' }
+            data={ 'text': '기존 목록에 신규 아이템' }
         )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
@@ -126,7 +126,7 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.post(
             '/lists/%d/' % (list_.id,),
-            data={'item_text': ''}
+            data={'text': ''}
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
