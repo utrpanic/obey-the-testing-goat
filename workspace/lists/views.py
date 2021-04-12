564d763e65dd0ae796552688cb4a7352d72020ae
box-jeon <utrpanic@gmail.com>
2021-04-13 01:02:08 +0900

    Chapter11. Form.save() 사용. Form에서 DB 저장을 한다니;;; 개선된 느낌은 아니다.

--- workspace/lists/views.py
+++ workspace/lists/views.py
@@ -5,16 +5,16 @@ from lists.forms import ItemForm
 from lists.models import Item, List
 
 def home_page(request):
-    return render(request, 'home.html', {'form': ItemForm()})
+    return render(request, 'home.html', {"form": ItemForm()})
 
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        item = Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
-        return render(request, 'home.html', {'form': form})
+        return render(request, 'home.html', {"form": form})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -22,6 +22,6 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            item = Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
