e837993626c7cc7d09bb8ee61c14be93da27028d
box-jeon <utrpanic@gmail.com>
2021-04-16 21:41:50 +0900

    Chapter13. 에러 메시지 초기화. 실패하는 테스트 추가.

--- workspace/functional_tests/test_list_item_validation.py
+++ workspace/functional_tests/test_list_item_validation.py
@@ -6,6 +6,9 @@ from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # 에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려고 한다.
         # 입력 상자가 비어 있는 상태에서 엔터키를 누른다.
@@ -16,7 +19,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는
         # 에러 메시지가 표시된다.
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.get_item_input_box().send_keys(' ') # required input 회피용.
         self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
@@ -33,7 +36,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 리스트 페이지에 다시 에러 메시지가 표시된다.
         self.check_for_row_in_list_table('1: 우유 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
         # 아이템을 입력하면 정상 동작한다.
@@ -58,5 +61,21 @@ class ItemValidationTest(FunctionalTest):
         
         # 도움이 되는 에러 메시지를 본다
         self.check_for_row_in_list_table('1: 콜라 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, DUPLICATE_ITEM_ERROR)
+
+    def test_error_messages_are_cleared_on_input(self):
+        # 에디스는 검증 에러를 발생시키도록 신규 목록을 시작한다
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys(' ') # required input 회피용.
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(0.5)
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # 에러를 제거하기 위해 입력 상자에 타이핑하기 시작한다
+        self.get_item_input_box().send_keys('a')
+
+        # 에러가 메시지가 사라진 것을 보고 기뻐한다
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
